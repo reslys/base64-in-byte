22615e7a9f010a7c4b9f51ce05bd0f9220e410ec
Resly Suniar <reslysuniar@gmail.com>
2020-01-20 10:51:46 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function formatBytes (bytes, decimals = 2) {
+  if (bytes === 0) return '0 Bytes';
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['bytes', 'kb', 'mb'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+module.exports = function b64inByte (base64) {
+  var stringLength = base64.length - 'data:image/png;base64,'.length;
+  var sizeInBytes = 4 * Math.ceil((stringLength / 3)) * 0.5624896334383812;
+  return formatBytes(sizeInBytes);
+};
