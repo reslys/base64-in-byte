a7f7dc0b3398b2ed1153116f89035a4a38207b5e
Resly Suniar <reslysuniar@gmail.com>
2020-01-20 11:54:34 +0700

    update feature, update readme

--- README.md
+++ README.md
@@ -6,15 +6,47 @@
 
 Get base64 size in byte
 
-### Installation
+## Installation
 ```sh
-npm install base64inbyte
+npm install @reslys/base64inbyte --save-dev
 ```
 
-### Usage
+## Usage
 ```sh
-import { b64inByte } from 'base64inbyte'
+import { formatBytes, b64inByte } from 'base64inbyte'
 
 const fileSize = b64inByte(yourBase64)
-console.log(fileSize) // 34.85 kb
+console.log(fileSize) // 3485
+
+const inBytes = formatBytes(fileSize)
+console.log(inBytes) // 34.85 kb
+```
+
+## Available Methods
+### b64inByte
+Get base64 size in byte
+
+```javascript
+/**
+ * Get base64 size in byte
+ * @param {base64} base64
+ *
+ * @return {integer} the size in bytes
+ */
+
+ b64inByte(base64)
+```
+
+### formatBytes
+Format integer of bytes to readable string such as 34.85 kb
+
+```javascript
+/**
+ * Format integer of file size
+ * @param {integer} bytes
+ *
+ * @return {string} formatted size
+ */
+
+ formatBytes(bytes)
 ```
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-module.exports = function formatBytes (bytes, decimals = 2) {
-  if (bytes === 0) return '0 Bytes';
-  const k = 1024;
-  const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ['bytes', 'kb', 'mb'];
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+export default {
+  formatBytes (bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes'
+    const k = 1024
+    const dm = decimals < 0 ? 0 : decimals
+    const sizes = ['bytes', 'kb', 'mb']
+    const i = Math.floor(Math.log(bytes) / Math.log(k))
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+  },
+  b64inByte (base64) {
+    var stringLength = base64.length - 'data:image/png;base64,'.length
+    var sizeInBytes = 4 * Math.ceil((stringLength / 3)) * 0.5624896334383812
+    return sizeInBytes
+  }
 }
-
-module.exports = function b64inByte (base64) {
-  var stringLength = base64.length - 'data:image/png;base64,'.length;
-  var sizeInBytes = 4 * Math.ceil((stringLength / 3)) * 0.5624896334383812;
-  return sizeInBytes
-};
